9a71c1aa50e917d61154cb72382ccb6b263cd05f
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-15 20:55:05 -0500

    Se hizo funcion _optionsDialogBox()

--- lib/widgets/camera_imagepicker_widget.dart
+++ lib/widgets/camera_imagepicker_widget.dart
@@ -1,6 +1,38 @@
 import 'package:flutter/material.dart';
 
 class Camera extends StatelessWidget {
+
+    Future<void> _optionsDialogBox() {
+        return showDialog(
+            context: context,
+            builder: (BuildContext context) {
+                return AlertDialog(
+                    content: SingleChildScrollView(
+                        child: ListBody(
+                            children: <Widget>[
+                                GestureDetector(
+                                    child: Text('Tomar fotografía'),
+                                    onTap: () {
+                                        
+                                    },
+                                ),
+                                Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                ),
+                                GestureDetector(
+                                    child: Text('Tomar fotografía'),
+                                    onTap: () {
+
+                                    },
+                                )
+                            ],
+                        ),
+                    ),
+                );
+            }
+        );
+    }
+
     @override
     Widget build(BuildContext context) {
         return Scaffold(
