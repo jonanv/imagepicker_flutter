01e804d85e1eb7c4c10d4fa259c95e4d8a30f649
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-10-15 20:48:01 -0500

    Se hizo codigo para widget de camera

--- lib/widgets/camera_imagepicker_widget.dart
+++ lib/widgets/camera_imagepicker_widget.dart
@@ -4,7 +4,15 @@ class Camera extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return Scaffold(
-            
+            appBar: AppBar(
+                title: Text('Usando la cámara con Flutter'),
+            ),
+            body: Center(
+                child: RaisedButton(
+                    child: Text('Click cámara'),
+                    onPressed: () {},
+                ),
+            ),
         );
     }
 }
